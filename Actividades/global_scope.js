f8ec1adf1f7c6a73a48d4c8caa23c0642a2b829e
Ramonrx86 <joseramon.perez@alumnos.ulagos.cl>
2024-09-04 19:32:12 -0400

    ejemplos

---
+++ Actividades/global_scope.js
@@ -0,0 +1,12 @@
+function comprar(){
+    console.log(`copraste el comic ${comic}`);
+    //compraste el comic X-Men
+}
+
+function leer(){
+    console.log(`estas leyendo ${comic}`);
+    //estas leyendo X-Men
+}
+
+comprar();
+leer();
---
+++ Actividades/scope.js
@@ -0,0 +1,9 @@
+function teamCap (){
+    var heroe = "Capitan America";
+    console.log(heroe);
+}
+
+function teamIronMan (){
+    var heore = "Iron Man";
+    console.log(heroe);
+}
