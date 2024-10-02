ab4d0ffa0f942d262f4fe126c198206fb586cbf3
Ramonrx86 <joseramon.perez@alumnos.ulagos.cl>
2024-10-01 21:24:44 -0300

    Ejercicio 5: Implementar Funciones al ejercicio 2 de laboratorio

---
+++ 1-Ejercicios/EJERN5.js
@@ -0,0 +1,50 @@
+// solicitar la contraseña
+function solicitar() {
+    return prompt("Ingresa una contraseña:");
+  }
+  
+  // validar la contraseña
+  function validar(password) {
+    const longitudMinima = 8;
+    const regexMayuscula = /[A-Z]/;
+    const regexNumero = /[0-9]/;
+    const regexEspecial = /[!@#$%^&*()_+\-{}\[\]:;<>.,?~\\]/;
+    let esValida = true;
+  
+    if (password.length < longitudMinima) {
+      console.log("Debe tener al menos 8 caracteres.");
+      esValida = false;
+    }
+  
+    if (!regexMayuscula.test(password)) {
+      console.log("Debe contener al menos una letra mayúscula.");
+      esValida = false;
+    }
+  
+    if (!regexNumero.test(password)) {
+      console.log("Debe contener al menos un número.");
+      esValida = false;
+    }
+  
+    if (!regexEspecial.test(password)) {
+      console.log("Debe contener al menos un carácter especial.");
+      esValida = false;
+    }
+  
+    return esValida;
+  }
+  
+  // resultado de la validación
+  function mostrar(esValida) {
+    if (esValida) {
+      alert("¡La contraseña es válida!");
+    } else {
+      alert("La contraseña no cumple con uno o más requisitos. Revisa la consola para más detalles");
+    }
+  }
+  
+  // Llamar a las funciones
+  let password = solicitar();
+  let esValida = validar(password);
+  mostrar(esValida);
+  
